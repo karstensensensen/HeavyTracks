c110e07ee0946d87e0656fd397678441f46d941a
karstensensensen <simonblsoerensen@gmail.com>
2022-11-17 00:01:24 +0100

    implemented multi element getters, and a getAllPlaylists function.

--- HeavyTracks/SpotifyContext.cs
+++ HeavyTracks/SpotifyContext.cs
@@ -10,6 +10,7 @@ using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Windows.Automation.Peers;
 using System.Windows.Ink;
 
 namespace HeavyTracks
@@ -54,17 +55,42 @@ namespace HeavyTracks
             return m_user_id;
         }
 
-        public static void getPlaylists()
+        public static Dictionary<string, Playlist> getPlaylists()
         {
-            var request = spotifyApiReq(HttpMethod.Get, $"users/{getUserId()}/playlists");
+            var items = getAllItems(HttpMethod.Get, $"users/{getUserId()}/playlists");
+            Dictionary<string, Playlist> playlists = new();
 
-            var response = sendApiReq(request);
-
-            if(response?.IsSuccessStatusCode ?? false)
+            foreach(var item in items)
             {
-                var obj = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                Playlist playlist = new(item["collaborative"]?.Value<bool>() ?? false, item["public"]?.Value<bool>() ?? false, 
+                    item["name"]?.ToString() ?? "INVALID", item["description"]?.ToString() ?? "", item["id"]?.ToString() ?? "");
 
-                Trace.WriteLine(obj);
+                fillPlaylist(ref playlist);
+
+                playlists[playlist.id] = playlist;
+            }
+
+            return playlists;
+        }
+
+        public static void fillPlaylist(ref Playlist target)
+        {
+            var items = getAllItems(HttpMethod.Get, $"playlists/{target.id}/tracks");
+
+            foreach(var item in items)
+            {
+                var track_item = item["track"];
+                string id = track_item["id"]?.ToString() ?? "";
+
+                if (target.tracks.ContainsKey(id))
+                    target.tracks[id].weight++;
+                else
+                {
+                    Trace.WriteLine(item);
+                    Track new_track = new(1, track_item["name"]?.ToString(), "NOT YET IMPLEMENTED", track_item["duration_ms"].Value<int>(), id);
+                    target.tracks[id] = new_track;
+                }
+
             }
 
         }
@@ -91,7 +117,7 @@ namespace HeavyTracks
             builder.Query = q.ToString();
 
             // open the constructed authorization url in the default browser.
-            System.Diagnostics.Process.Start("explorer", $"\"{builder.ToString()}\"");
+            System.Diagnostics.Process.Start("explorer", $"\"{builder}\"");
 
             // prepare the local webserver, to retreive the authentication token.
             var listener = new HttpListener();
@@ -139,7 +165,7 @@ namespace HeavyTracks
             }
         }
 
-        private static string m_client_id;
+        private static string m_client_id = "";
         private static string? m_user_token;
         private static string? m_user_id = null;
 
@@ -149,6 +175,8 @@ namespace HeavyTracks
         private static readonly Uri API_ENDPOINT = new("https://api.spotify.com/v1/");
         private static readonly uint PORT = 8888;
         private static readonly string SCOPE = "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public";
+        private static readonly int MAX_LIMIT = 2;
+        private static readonly int MAX_OFFSET = 100_000;
 
         /// <summary>
         /// constructs a HttpRequestMessage with the specified method.
@@ -183,7 +211,7 @@ namespace HeavyTracks
             if (msg == null)
                 return null;
 
-            var response = m_client.Send(msg);
+            var response = m_client.SendAsync(msg).Result;
 
             if (response.StatusCode == HttpStatusCode.Unauthorized)
             {
@@ -194,24 +222,99 @@ namespace HeavyTracks
 
             return response;
         }
+
+        /// <summary>
+        /// retrieves all items of a spotify endpoint, which returns a variable number of items.
+        /// </summary>
+        /// <param name="method"> request method </param>
+        /// <param name="endpoint"> spotify api endpoint, which returns a variable number of items</param>
+        /// <param name="query"> additional query parameters </param>
+        /// <returns> list of the varaible json items returned from the spotify api </returns>
+        private static List<JObject> getAllItems(HttpMethod method, string endpoint, string query = "")
+        {
+            /// a spotify query with a list type of result, will only return a maximum of 50 entries per request.
+            /// in order to recieve all of the values in for example a playlist,
+            /// get requests will be sent, with an increasing offset of 50, will be sent, until all playlist tracks have been retrieved.
+            /// if this function fails at any point, an empty list will be returned.
+
+            List<JObject> items = new();
+
+            int pages = 0;
+
+            while (pages * MAX_LIMIT < MAX_OFFSET)
+            {
+                var msg = spotifyApiReq(method, endpoint);
+                
+                if (msg == null)
+                    break;
+                
+                var builder = new UriBuilder(msg.RequestUri ?? new(""));
+                var item_query = HttpUtility.ParseQueryString(query);
+
+                item_query["limit"] = MAX_LIMIT.ToString();
+                item_query["offset"] = (MAX_LIMIT * pages).ToString();
+                builder.Query = item_query.ToString();
+
+                msg.RequestUri = builder.Uri;
+
+
+                var response = sendApiReq(msg);
+
+                if(response == null || !response.IsSuccessStatusCode) break;
+
+                var content = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+
+                var recieved_items = content["items"]?.ToArray();
+
+                foreach (var item in recieved_items ?? Enumerable.Empty<JToken>())
+                    items.Add((JObject)item);
+
+                if (content["total"]?.Value<int>() - pages * MAX_LIMIT < MAX_LIMIT)
+                    break;
+
+                pages++;
+            }
+
+            return items;
+        }
     }
 
     public class Track
     {
+        public Track(int _weight, string _name, string _artist, int _length, string _id)
+        {
+            weight = _weight;
+            name = _name;
+            artist = _artist;
+            length = _length;
+            id = _id;
+        }
+
         public int weight;
         public string name;
         public string artist;
+        public string id;
         public int length;
     }
 
-    public class SpotifyTrack
+    public class Playlist
     {
+        public Playlist(bool _is_collaborative, bool _is_public, string _name, string _description, string _id)
+        {
+            is_collaborative = _is_collaborative;
+            is_public = _is_public;
+            name = _name;
+            description = _description;
+            id = _id;
+        }
+
+
         public bool is_collaborative;
         public bool is_public;
         public string name;
         public string description;
         public string id;
 
-        List<Track> tracks;
+        public Dictionary<string, Track> tracks = new();
     }
 }
