06cd6a193e33c2bc3d88f4879efecae5666352b1
karstensensensen <simonblsoerensen@gmail.com>
2022-12-16 09:01:22 +0100

    setup view model development

--- HeavyTracks/ViewModels/HeavyTracksVM.cs
+++ HeavyTracks/ViewModels/HeavyTracksVM.cs
@@ -1,3 +1,4 @@
+using HeavyTracks.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,6 +7,6 @@ namespace HeavyTracks.ViewModels
 {
     public class HeavyTracksVM : ViewModelBase
     {
-
+        List<Playlist> Playlists { get; set; }
     }
 }
